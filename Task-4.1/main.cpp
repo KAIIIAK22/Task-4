50416b039dc8d10e73e530a422f43fa83d95faa2
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-21 20:35:37 +0300

    Update main.cpp

--- Task-4.1/main.cpp
+++ Task-4.1/main.cpp
@@ -1,26 +1,15 @@
 #include <iostream>
 
 using namespace std;
-
-int counting(int maxim, int ten)
+int counting(int maxim, int ten=1)
 {
-    int ret=0;
-    if (maxim/ten>=10)
-    {
-        ret=counting(maxim, ten*10);
-        while(maxim/ten>=10)
-        {
-           maxim -=ten*10;
-        }
-        return ret+(int)maxim/ten;
-    }
+    if (maxim/ten>=10) return counting(maxim, ten*10)+(maxim/ten)%10;
     else return (int) (maxim/ten);
-
-
 }
+
 int main()
 {
     int start;
     cin >> start;
-    cout <<counting(start,1);
+    cout <<counting(start);
 }
