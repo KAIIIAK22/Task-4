b4e908ddf26edbc3f9e3c61f815de74674c6de26
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-30 20:22:36 +0300

    Functions that making tree

--- Task-4.2/main.cpp
+++ Task-4.2/main.cpp
@@ -1,23 +1,46 @@
 #include <iostream>
 
 using namespace std;
-void elxs (int a,int b=1)
+
+void StarLine (int length)
 {
-    for (int i =0;i<a;i++) cout <<" ";
-    for (int i =0;i<b;i++) cout <<"*";
-    cout <<endl;
-    if (a>1) elxs(a-1,b+2);
-    if (b==1)
+    for(;length>0;length--) cout << "*";
+}
+
+void SpaceLine (int length)
+{
+    for(;length>0;length--) cout << " ";
+}
+int ElxSize ()
+{
+    int Elxsize;
+    cout << "Size:";
+    cin >> Elxsize;
+    return Elxsize;
+}
+
+void DrawElxLeaves (int SiZe)
+{
+    int b = SiZe;
+    for (int i = 0;i<SiZe;i++)
     {
-        for (int i =0;i<a;i++) cout <<" ";
-        cout <<"*";
+        SpaceLine (b);
+        StarLine (1+i*2);
+        cout << endl;
+        b--;
     }
+}
+
+void DrawLog(int SiZe)
+{
+    int b = SiZe;
+    SpaceLine (b);
+    StarLine (1);
 }
 
 int main()
 {
-
-    int start;
-    cin >> start;
-    elxs(start);
+    int leng = ElxSize();
+    DrawElxLeaves(leng);
+    DrawLog(leng);
 }
